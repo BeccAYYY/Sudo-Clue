9506a0db6e999c4a62137c5e418dde783174bbff
BeccAYYY <becsell@live.com.au>
2021-09-30 12:54:11 +1000

    Added code graveyard while continuing to refactor

--- js.js
+++ js.js
@@ -1,11 +1,22 @@
+/*
+Definitions
+
+Box: A single cell of the sudoku grid
+Square: A 3x3 square in the sudoku grid
+Candidate: A number that can go in an unsolved box
+Coordinates: A pair of two values that give the location of a box in the sudoku array (0-indexed). Referred to a "x" for horizontal and "y" for vertical.
+
+*/
+
 var sudoku = new Array;
-var candidates = new Array;
+var puzzleCandidates = new Array;
+var oneThroughNine = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function reset() {
     sudoku = [
     [1, 2, 3, 0, 0, 0, 0, 0, 0],
     [4, 5, 6, 0, 0, 0, 0, 0, 0],
-    [7, 8, 9, 0, 0, 0, 0, 0, 0],
+    [7, 8, 9, 0, 0, 0, 2, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,7 +24,7 @@ function reset() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
-var a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+var a = oneThroughNine;
 var b = [4, 5, 6, 7, 8, 9]
 var c = [1, 2, 3, 7, 8, 9]
 var d = [1, 2, 3, 4, 5, 6]
@@ -21,10 +32,10 @@ var e = [2, 3, 5, 6, 8, 9]
 var f = [1, 3, 4, 6, 7, 9]
 var g = [1, 2, 4, 5, 7, 8]
 
-    candidates = [
-        [[1],[2],[3], b, b, b, b, b, b],
-        [[4],[5],[6], c, c, c, c, c, c],
-        [[7],[8],[9], d, d, d, d, d, d],
+    puzzleCandidates = [
+        [0, 0, 0, b, b, b, b, b, b],
+        [0, 0, 0, c, c, c, c, c, c],
+        [0, 0, 0, d, d, d, d, d, d],
         [e, f, g, a, a, a, a, a, a],
         [e, f, g, a, a, a, a, a, a],
         [e, f, g, a, a, a, a, a, a],
@@ -35,8 +46,8 @@ var g = [1, 2, 4, 5, 7, 8]
 }
 reset();
 
-console.log(candidates);
 
+//This function returns an array of three numbers that will be either the x or y coordinates in the same 3x3 square as the x or y coordinate given for either a row or column.
 function getSquare(x) {
     if (x <= 2) {
     return [0, 1, 2]
@@ -46,36 +57,14 @@ function getSquare(x) {
     return [6, 7, 8]
 }};
 
-function getBasicBoxCandidates(x, y) {
-    var candidates = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    sudoku.forEach(row => {
-        if (row[x] !== 0) {
-            candidates.splice(candidates.indexOf(row[x]), 1);
-        }
-    })
-    sudoku[y].forEach(box => {
-        if (box !== 0 && candidates.indexOf(box) !== -1) {
-            candidates.splice(candidates.indexOf(box), 1);
-        }
-    })
-    var squareX = getSquare(x);
-    var squareY = getSquare(y);
-    squareY.forEach(yIndex => {
-        squareX.forEach(xIndex => {
-            if (sudoku[yIndex][xIndex] !== 0 && candidates.indexOf(sudoku[yIndex][xIndex]) !== -1) {
-                candidates.splice(candidates.indexOf(sudoku[yIndex][xIndex]), 1)
-            }
-        })
-    })
-    return candidates;
-}
 
-function getRowCandidates(x, y) {
+//Takes coordinates of a box and returns an array of the candidates for each item in the row that is not solved and is not the box for which coordinates were given.
+function getExclusiveRowCandidates(x, y) {
     var rowCandidates = [];
-    sudoku[y].forEach((box, xIndex) => {
+    puzzleCandidates[y].forEach((array, xIndex) => {
         if (xIndex !== x) {
-        if (box == 0) {
-            rowCandidates.push(getBasicBoxCandidates(xIndex, y));
+        if (array !== 0) {
+            rowCandidates.push(puzzleCandidates[y][xIndex]);
         }
     }
     })
@@ -83,27 +72,30 @@ function getRowCandidates(x, y) {
 }
 
 
-function getColumnCandidates(x, y) {
+//Takes coordinates of a box and returns an array of the candidates for each item in the column that is not solved and is not the box for which coordinates were given.
+function getExclusiveColumnCandidates(x, y) {
     var columnCandidates = [];
-    sudoku.forEach((row, yIndex) => {
+    puzzleCandidates.forEach((row, yIndex) => {
         if (yIndex !== y) {
-        if (row[x] == 0) {
-            columnCandidates.push(getBasicBoxCandidates(x, yIndex));
+        if (row[x] !== 0) {
+            columnCandidates.push(puzzleCandidates[yIndex][x]);
         }
     }
     })
     return columnCandidates;
 }
 
-function getSquareCandidates(x, y) {
+
+//Takes coordinates of a box and returns an array of the candidates for each item in the 3x3 square that is not solved and is not the box for which coordinates were given.
+function getExclusiveSquareCandidates(x, y) {
     var squareCandidates = [];
     var squareX = getSquare(x);
     var squareY = getSquare(y);
     squareY.forEach(yIndex => {
         squareX.forEach(xIndex => {
             if (yIndex !== y || xIndex !== x) {
-                if (sudoku[yIndex][xIndex] == 0) {
-                    squareCandidates.push(getBasicBoxCandidates(xIndex, yIndex))
+                if (puzzleCandidates[yIndex][xIndex] !== 0) {
+                    squareCandidates.push(puzzleCandidates[yIndex][xIndex])
                 }
         }
         })
@@ -111,6 +103,8 @@ function getSquareCandidates(x, y) {
     return squareCandidates;
 }
 
+
+//Counts the number of times a single candidate appears in an array of candidates.
 function countCandidateOccurences(n, array) {
     var count = 0;
     array.forEach(candidateSet => {
@@ -121,40 +115,75 @@ function countCandidateOccurences(n, array) {
     return count;
 }
 
-function fillIfOnlyOneCandidate(x, y) {
-    if (sudoku[y][x] == 0) {
-        var fill = 0;
-        var change = false;
-        var candidates = getAdvancedBoxCandidates(x, y);
-        if (candidates.length == 1) {
-            fill = candidates[0];
+
+//Takes coordinates, gets the solved value for those coordinates and removes it from the candidates list for the column, row and square that the solved box is is.
+function changeCandidatesGridAfterFill(x, y, n) {
+    console.log("changeCandidatesGridAfterFill")
+    puzzleCandidates[y][x] = 0;
+    var squareY = getSquare(y);
+    var squareX = getSquare(x);
+    puzzleCandidates.forEach((row, yIndex) => {
+        if (squareY.includes(yIndex)) {
+            if (yIndex === y) {
+                row.forEach((boxCandidates, xIndex) => {
+                    if (Array.isArray(boxCandidates) && xIndex !== x && boxCandidates.includes(n)) {
+                        puzzleCandidates[y][xIndex] = boxCandidates.filter(candidate => candidate !== n);
+                    }
+                })
+            } else {
+                squareX.forEach(xIndex => {
+                    if (Array.isArray(row[xIndex]) && row[xIndex].includes(n)) {
+                        row[xIndex] = row[xIndex].filter(candidate => candidate !== n)
+                    }
+                })
+            }
+        } else {
+            if (Array.isArray(row[x]) && row[x].includes(n)) {
+                row[x] = row[x].filter(candidate => candidate !== n)
+            }
         }
-        sudoku[y][x] = fill;
-        if (fill !== 0) {
-            change = true;
-        }
-        return change;
-    }
+    })
 }
 
 
+//Fills a box and updates the candidate grid
+function fillBox(x, y, n) {
+    sudoku[y][x] = n;
+    changeCandidatesGridAfterFill(x, y, n)
+}
+
+
+function fillSingleCandidateBoxes() {
+    do {
+        var change = false;
+        yIndex = 0;
+        xIndex = 0;
+        while (!change) {
+            while (!change)
+            yIndex++
+        }
+
+    }
+    while (change);
+}
+/*
 function fillIfRequired(x, y) {
     if (sudoku[y][x] == 0) {
     var fill = 0;
     var change = false;
     var candidates = getAdvancedBoxCandidates(x, y);
     candidates.forEach(candidate => {
-        var rowCandidates = getRowCandidates(x, y);
+        var rowCandidates = getExclusiveRowCandidates(x, y);
         var count = countCandidateOccurences(candidate, rowCandidates)
         if (count == 0) {
             fill = candidate;
         }
-        var columnCandidates = getColumnCandidates(x, y);
+        var columnCandidates = getExclusiveColumnCandidates(x, y);
         var count = countCandidateOccurences(candidate, columnCandidates)
         if (count == 0) {
             fill = candidate;
         }
-        var squareCandidates = getSquareCandidates(x, y);
+        var squareCandidates = getExclusiveSquareCandidates(x, y);
         var count = countCandidateOccurences(candidate, squareCandidates)
         if (count == 0) {
             fill = candidate;
@@ -215,10 +244,10 @@ function fillGrid() {
 
 fillGrid();
 
-function countSuccess() {
+function countSuccess(n) {
     var successes = 0;
     var failures = 0;
-    for (let i = 0; i < 200; i++) {
+    for (let i = 0; i < n; i++) {
         console.log(i);
         if (fillGrid()) {
             successes++;
@@ -227,19 +256,19 @@ function countSuccess() {
         }
         reset();
     }
-    console.log("Successful Sudoku created " + successes + " times, and unsuccessful " + failures + " times. (" + successes/2 + "%)");
+    console.log("Successful Sudoku created " + successes + " times, and unsuccessful " + failures + " times. (" + successes/n * 100 + "%)");
 }
 
-//countSuccess();
+//countSuccess(1000);
 
 
 
 
 function getAdvancedBoxCandidates(x, y) {
     var basicCandidates= getBasicBoxCandidates(x, y);
-    var columnNakedSubsets = findNakedSubsets(getColumnCandidates(x, y));
-    var rowNakedSubsets = findNakedSubsets(getRowCandidates(x, y));
-    var squareNakedSubsets = findNakedSubsets(getSquareCandidates(x, y))
+    var columnNakedSubsets = findNakedSubsets(getExclusiveColumnCandidates(x, y));
+    var rowNakedSubsets = findNakedSubsets(getExclusiveRowCandidates(x, y));
+    var squareNakedSubsets = findNakedSubsets(getExclusiveSquareCandidates(x, y))
     var advancedCandidates = basicCandidates;
     if (columnNakedSubsets) {
         advancedCandidates = removeCandidatesFromArray(advancedCandidates, columnNakedSubsets)
@@ -266,9 +295,9 @@ function removeCandidatesFromArray(initialArray, candidatesToRemoveArray) {
 function findNakedSubsets(candidatesArray) {
     var subset = false;
     for (let i = 0; i < candidatesArray.length; i++) {
+        var matches = 0;
         for (let n = 0; n < candidatesArray.length; n++) {
-            if (n !== i && candidatesArray[i].length == candidatesArray[n].length) {
-                var matches = 0;
+            if (n !== i && candidatesArray[i].length === candidatesArray[n].length) {
                 if (candidatesArray[i].length == 2) {
                     if (checkIfArraysMatch(candidatesArray[i], candidatesArray[n])) {
                         subset = candidatesArray[i];
@@ -290,6 +319,22 @@ function findNakedSubsets(candidatesArray) {
                         subset = candidatesArray[i];
                         return subset;
                     }
+                } else if (candidatesArray[i].length == 5) {
+                    if (checkIfArraysMatch(candidatesArray[i], candidatesArray[n])) {
+                        matches++
+                    }
+                    if (matches == 4) {
+                        subset = candidatesArray[i];
+                        return subset;
+                    }
+                } else if (candidatesArray[i].length == 6) {
+                    if (checkIfArraysMatch(candidatesArray[i], candidatesArray[n])) {
+                        matches++
+                    }
+                    if (matches == 5) {
+                        subset = candidatesArray[i];
+                        return subset;
+                    }
                 }
             }
         }
@@ -307,3 +352,15 @@ function checkIfArraysMatch(array1, array2) {
 }
 }
 
+var test = [[1,2,3], [1,2,3,4], [1,2,4], [4,5,6,7,8,9], [5,6,7,8], [4, 5, 6, 9], [4, 5, 6, 9]]
+
+function findHiddenSubsets(candidatesArray) {
+    var candidatesCount = [0,0,0,0,0,0,0,0,0];
+    oneThroughNine.forEach((n, i) => {
+        candidatesCount[i] = countCandidateOccurences(n, candidatesArray);
+    })
+    return candidatesCount;
+}
+
+console.log(findHiddenSubsets(test));
+*/
