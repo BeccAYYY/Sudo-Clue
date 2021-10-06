81e83d62f3eee68fedb5daa95b2754621870e57e
BeccAYYY <becsell@live.com.au>
2021-10-06 11:28:41 +1000

    fixed puzzle creator

--- js.js
+++ js.js
@@ -13,21 +13,10 @@ Constraints: The rules of a Sudoku that require each digit to appear only once i
 
 */
 
-var puzzle = new Array;
-var puzzleCandidates = new Array;
-var groups = new Array;
-var oneThroughNine = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-reset();
+/*
 
-createGroupsArray();
-
-
-countSuccess(1000);
-
-
-
-/*var sudoku = [
+var sudoku = [
     [8, 0, 0, 4, 0, 6, 0, 0, 7],
     [0, 0, 0, 0, 0, 0, 4, 0, 0],
     [0, 1, 0, 0, 0, 0, 6, 5, 0],
@@ -40,19 +29,7 @@ countSuccess(1000);
 ];
 
 
+*/
 
-var methods = {
-    "Lone Rangers": true, 
-    "Locked Candidates": true, 
-    "Naked Subsets": true, 
-    "Hidden Subsets": true
-};
 
-var groups = new Array;
 
-var oneThroughNine = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-createGroupsArray()
-var puzzleCandidates = createCandidatesGridForPuzzle(sudoku)
-
-console.log(checkIfSolvable())*/
